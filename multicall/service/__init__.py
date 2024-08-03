09400c882af19aca8b40ba9f3c1e0905a6d0c012
jsvisa <delweng@gmail.com>
2024-08-03 11:47:06 +0800

    chore(service): add __all__
    Signed-off-by: jsvisa <delweng@gmail.com>

--- multicall/service/__init__.py
+++ multicall/service/__init__.py
@@ -1,5 +1,13 @@
-from .erc20_service import Erc20Service  # noqa: F401
-from .token_service import TokenService  # noqa: F401
-from .ether_service import EtherService  # noqa: F401
-from .erc721_service import Erc721Service  # noqa: F401
-from .erc1155_service import Erc1155Service  # noqa: F401
+from .erc20_service import Erc20Service
+from .token_service import TokenService
+from .ether_service import EtherService
+from .erc721_service import Erc721Service
+from .erc1155_service import Erc1155Service
+
+__all__ = [
+    "Erc20Service",
+    "TokenService",
+    "EtherService",
+    "Erc721Service",
+    "Erc1155Service",
+]
