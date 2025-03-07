8abbf61f5046d11ec514773e0b8392c3814c7718
jsvisa <delweng@gmail.com>
2025-03-07 11:55:52 +0800

    tests: add decode bytes without zip the inner tuple array
    Signed-off-by: jsvisa <delweng@gmail.com>

--- tests/testdata/__init__.py
+++ tests/testdata/__init__.py
@@ -1,11 +1,13 @@
 from .swapETHForSpecificNFTS import ABI_swapETHForSpecificNFTs
 from .matchAdvancedOrders import ABI_matchAdvancedOrders
 from .claim import ABI_claim
+from .execute import ABI_execute
 from .swapExactTokenForPt import ABI_swapExactTokenForPt
 
 __all__ = [
     "ABI_swapETHForSpecificNFTs",
     "ABI_matchAdvancedOrders",
     "ABI_claim",
+    "ABI_execute",
     "ABI_swapExactTokenForPt",
 ]
---
+++ tests/testdata/execute.py
@@ -0,0 +1,11 @@
+ABI_execute = {
+    "name": "execute",
+    "type": "function",
+    "inputs": [
+        {"name": "commands", "type": "bytes", "internalType": "bytes"},
+        {"name": "inputs", "type": "bytes[]", "internalType": "bytes[]"},
+        {"name": "deadline", "type": "uint256", "internalType": "uint256"},
+    ],
+    "outputs": [],
+    "stateMutability": "payable",
+}
