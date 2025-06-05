5aeac3d40008a6bcf920dfe85962360467747bd9
Delweng <delweng@gmail.com>
2025-06-05 23:50:37 +0800

    Merge pull request #3 from jsvisa/fix/tuple-out-of-order
    fix: n-dimension tuple can't be decoded

---
+++ tests/testdata/smartSwapByOrderId.py
@@ -0,0 +1,51 @@
+ABI_smartSwapByOrderId = {
+    "name": "smartSwapByOrderId",
+    "type": "function",
+    "inputs": [
+        {"name": "orderId", "type": "uint256"},
+        {
+            "name": "baseRequest",
+            "type": "tuple",
+            "components": [
+                {"name": "fromToken", "type": "uint256"},
+                {"name": "toToken", "type": "address"},
+                {"name": "fromTokenAmount", "type": "uint256"},
+                {"name": "minReturnAmount", "type": "uint256"},
+                {"name": "deadLine", "type": "uint256"},
+            ],
+            "internalType": "struct DexRouter.BaseRequest",
+        },
+        {"name": "batchesAmount", "type": "uint256[]"},
+        {
+            "name": "batches",
+            "type": "tuple[][]",
+            "components": [
+                {"name": "mixAdapters", "type": "address[]"},
+                {"name": "assetTo", "type": "address[]"},
+                {"name": "rawData", "type": "uint256[]"},
+                {"name": "extraData", "type": "bytes[]"},
+                {"name": "fromToken", "type": "uint256"},
+            ],
+            "internalType": "struct DexRouter.RouterPath[][]",
+        },
+        {
+            "name": "extraData",
+            "type": "tuple[]",
+            "components": [
+                {"name": "pathIndex", "type": "uint256"},
+                {"name": "payer", "type": "address"},
+                {"name": "fromToken", "type": "address"},
+                {"name": "toToken", "type": "address"},
+                {"name": "fromTokenAmountMax", "type": "uint256"},
+                {"name": "toTokenAmountMax", "type": "uint256"},
+                {"name": "salt", "type": "uint256"},
+                {"name": "deadLine", "type": "uint256"},
+                {"name": "isPushOrder", "type": "bool"},
+                {"name": "extension", "type": "bytes"},
+            ],
+            "internalType": "struct PMMLib.PMMSwapRequest[]",
+        },
+    ],
+    "outputs": [{"name": "returnAmount", "type": "uint256", "internalType": "uint256"}],
+    "stateMutability": "payable",
+}
